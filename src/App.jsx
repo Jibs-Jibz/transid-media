c1441e66f0bba8efa342e892384b5aafb298c303
osunkoyajibola <ajibola.osunkoya@thebulb.africa>
2023-03-12 17:49:51 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import MaintenancePage from "./components/MaintenancePage";
+
+function App() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#242424",
+        //  maxHeight: "100vh"
+      }}
+    >
+      <MaintenancePage />
+    </div>
+  );
+}
+
+export default App;
